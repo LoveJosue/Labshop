b0b1c9dba8928bc9c656a4bac37e92dd01cabb6b
LoveJosue <josue.avlah@outlook.com>
2025-05-28 15:15:51 -0400

    Fixing production config issues

--- Server/app.js
+++ Server/app.js
@@ -3,34 +3,26 @@ import products from './routes/products.js';
 import express from 'express';
 import logger from './middleware/logger.js'
 import cors from 'cors';
-import { createServer } from '@vercel/node';
+
 config();
-const port = process.env.PORT || 3000;
 
 const app = express();
 
-// Autorisation des requettes venant du front-end
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
+// Restrictions d'accès à l'API
+// app.use(cors({
+//     origin: 'http://localhost:5173'
+// }))
 
-// Temporairement (autoriser tous les clients)
-// app.use(cors());
-
+// Autoriser tous les domaines. Restreindre plus tard
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// Routes
-app.use('/api/products', products);
-
 // Logger middleware
 app.use(logger);
 
+// Routes
+app.use('/api/products', products);
 
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
-
-export default createServer(app);
+export default app;
